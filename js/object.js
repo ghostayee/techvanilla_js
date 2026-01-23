cd899c4f0a8ab7a521ceda1101708cd6e49d47ea
ghostayee <mayeeayee@outlook.com>
2026-01-23 20:30:40 +0300

    weekend

--- js/object.js
+++ js/object.js
@@ -2,7 +2,7 @@ let person ={
     "name": "Jane Doe",
     "age": 28,
     "address": "123 New York",
-    "is_student": true
+    "is student": true
 }
 console.log(person)
 console.log(typeof(person)) // object
@@ -15,3 +15,18 @@ console.log(person.address)
 
 // bracket notation allow it when have spaces in key
 console.log(person["is student"])
+
+//adding values - bracket notation
+person["job"] = "developer"
+
+//dot notation
+person.country = "usa"
+console.log(person)
+
+// updating values
+person.age = 29
+console.log(person)
+
+console.log(Object.keys(person))
+console.log(Object.values(person))
+console.log(Object.entries(person))
