8e5c262f6e7ce2b98003adc358517aeac9d7b298
ghostayee <mayeeayee@outlook.com>
2026-01-23 20:08:29 +0300

    push all the propert

--- js/object.js
+++ js/object.js
@@ -1,7 +1,17 @@
 let person ={
-    name: "Jane Doe",
-    age: 28,
-    address: "123 New York"
+    "name": "Jane Doe",
+    "age": 28,
+    "address": "123 New York",
+    "is_student": true
 }
 console.log(person)
-console.log(typeof(person))
+console.log(typeof(person)) // object
+
+// accessing values
+console.log(person.name)
+console.log(person.age) //25
+console.log(person.address)
+
+
+// bracket notation allow it when have spaces in key
+console.log(person["is student"])
